f411f317bc326319e6fe420630d028c5e8db60c0
rasenin <romansenin15@gmail.com>
2019-04-20 20:15:08 -0700

    added more styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -22,18 +22,21 @@ $.each(topics, function(_, food) {
 $(document).on("click", ".button", requestAPI);
 
 function requestAPI() {
+  $(".message").hide();
   $(".gifdump").empty();
 
   let queryURL =
     "https://api.giphy.com/v1/gifs/search?q=" +
     $(this).val() +
-    "&limit=10&rating=g&api_key=iEDFIwNeXtpxGTMckg0Oo79DtSZrJFii";
+    "&limit=10&rating=pg&api_key=iEDFIwNeXtpxGTMckg0Oo79DtSZrJFii";
 
   $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
+    console.log(response);
     for (let i = 0; i < response.data.length; i++) {
+      let newGifDiv = $("<div>").addClass("gifdiv");
       let newGif = $("<img>");
       newGif
         .attr("src", response.data[i].images.original_still.url)
@@ -41,7 +44,9 @@ function requestAPI() {
         .attr("data-still", response.data[i].images.original_still.url)
         .attr("data-animate", response.data[i].images.original.url)
         .addClass("gif");
-      $(".gifdump").append(newGif);
+      newGifDiv.append(newGif);
+      newGifDiv.append("<h5 class='rating'> Rating: " + response.data[i].rating + "</h5>")
+      $(".gifdump").append(newGifDiv);
     }
   });
 }
